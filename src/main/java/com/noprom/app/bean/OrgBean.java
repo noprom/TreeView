3c8aa53bc9a78af2223d72ddcea505c00fa90b18
noprom <874702417@qq.com>
2015-02-16 09:49:51 +0800

    change FileBean and OrgBean
    change FileBean and OrgBean

--- src/main/java/com/noprom/app/bean/OrgBean.java
+++ src/main/java/com/noprom/app/bean/OrgBean.java
@@ -5,43 +5,11 @@ package com.noprom.app.bean;
  * 根据ListView's item + paddingLeft(level) + expand include
  * 将系统中的数据Bean -> Node
  *
+ *
  * Created by noprom on 2015/2/16.
  */
 public class OrgBean {
-    private int id;
-    private int pid;
-    private String label;
-    private String desc;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public int getPid() {
-        return pid;
-    }
-
-    public void setPid(int pid) {
-        this.pid = pid;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
+    private int _id;
+    private int parentId;
+    private String name;
 }
--- src/main/java/com/noprom/app/utils/TreeHelper.java
+++ src/main/java/com/noprom/app/utils/TreeHelper.java
@@ -1,21 +1,31 @@
 package com.noprom.app.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 树形控件管理器
  * Created by noprom on 2015/2/16.
+ *
  */
 public class TreeHelper {
 
+
     /**
      * 将数据转化为节点
+     * 通过反射与注解获得某个Bean的id,pid和name
+     * @param datas
      * @param <T>
      * @return
      */
-    public static <T> List<T> convertDatasToNodes(){
+    public static <T> List<Node> convertDatasToNodes(List<T> datas) {
 
+        List<Node> nodes = new ArrayList<Node>();
+        Node node = null;
+        for (T t:datas){
+            node = new Node();
 
+        }
         return null;
     }
 }
